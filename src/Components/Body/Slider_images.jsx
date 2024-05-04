4fb7278587575228c23f585af11a6f8844dfe154
Parth <parthjohi009@gmail.com>
2024-05-04 17:36:08 +0530

    SliderLast

--- src/Components/Body/Slider_images.jsx
+++ src/Components/Body/Slider_images.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import './Slider_images.css'; 
+import React, { useState, useEffect } from "react";
+import "./Slider_images.css";
 
-import image1 from './Slider_images/WhatsApp Image 2024-05-04 at 09.11.40_4ce8c3c3.jpg';
-import image2 from './Slider_images/WhatsApp Image 2024-05-04 at 09.11.40_9f76b04b.jpg';
-import image3 from './Slider_images/WhatsApp Image 2024-05-04 at 09.11.40_c2776e76.jpg';
+import image1 from "./Slider_images/WhatsApp Image 2024-05-04 at 09.11.40_4ce8c3c3.jpg";
+import image2 from "./Slider_images/WhatsApp Image 2024-05-04 at 09.11.40_9f76b04b.jpg";
+import image3 from "./Slider_images/WhatsApp Image 2024-05-04 at 09.11.40_c2776e76.jpg";
 
 const images = [image1, image2, image3];
 
@@ -25,19 +25,46 @@ const Slider = () => {
   useEffect(() => {
     const interval = setInterval(() => {
       nextSlide();
-    }, 3000); 
+    }, 3000);
 
-    return () => clearInterval(interval); 
-
+    return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="slider-container">
-      <div className="image-container">
-        <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} />
+    <div className="slider-container ">
+      <div className="image-container zindex">
+        <img src={images[currentIndex]} alt={`Slide ${currentIndex}`}   />
       </div>
-      <button className="prev-button" onClick={prevSlide}>Prev</button>
-      <button className="next-button" onClick={nextSlide}>Next</button>
+      <button className="prev-button" onClick={prevSlide}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-arrow-left"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+          />
+        </svg>
+      </button>
+      <button className="next-button" onClick={nextSlide}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-arrow-right"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+          />
+        </svg>
+      </button>
     </div>
   );
 };
