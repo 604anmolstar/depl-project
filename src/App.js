ed38af4825fb3ca98b54c7c266feaa153af15fba
Parth <parthjohi009@gmail.com>
2024-05-04 11:50:22 +0530

    Silder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
+import { Main_image } from "./Components/Body/Slider_images.jsx";
+import Navbar from "./Components/Navbar/Navbar.jsx";
 
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
     <Navbar />
+    <Main_image />
     </>
   )
 }
